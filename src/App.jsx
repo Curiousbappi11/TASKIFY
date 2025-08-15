1c05f4237c0583ad85cedceb67666ceae15958fe
BAPPI SINGHA <singhabappi11@gmail.com>
2025-08-15 17:25:21 +0530

    feat: initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-dvw h-dvh bg-[#291C0E] text-[#fff]'>
+      
+    </div>
+  )
+}
+
+export default App
