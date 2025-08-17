cf618d443df0c3a0e7466195534e1dbdfa1ea1b3
BAPPI SINGHA <singhabappi11@gmail.com>
2025-08-18 03:10:23 +0530

    feat: add task management components and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
+import Tasks from './Components/Tasks'
 // import './App.css'
 
 function App() {
 
   return (
-    <div className='w-dvw h-dvh bg-[#291C0E] text-[#fff]'>
-      
+    <>
+    <div className='w-dvw h-dvh flex flex-col justify-center items-center p-7 bg-[#d3d3d3] text-[#444]'>
+      <Tasks />
     </div>
+    </>
   )
 }
 
 export default App
+
+/*
+#3E2522
+#8C6E63
+#D3A376
+#FFE0B2
+#FFF2DF
+*/
---
+++ src/Components/Tasks.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import TaskItems from './TaskItems'
+
+function Tasks() {
+  return (
+    <>
+      {/* main container of Tasks */}
+      <div className='w-[20rem]'>
+
+        {/* heading */}
+        <h1 className='font-black text-3xl mb-4'>Your To Do</h1>
+
+        {/* form box */}
+        <form action="">
+
+          {/* input container */}
+          <div className='flex gap-4 mb-6'>
+
+            {/* input field */}
+            <input type="text" name="" id="" placeholder='Add new task' className='border-b-1 px-1 flex-1 outline-none' />
+
+            {/* create button */}
+            <button className='bg-[#444] p-1 rounded-lg'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+            </button>
+
+          </div>
+
+          {/* All Tasks items */}
+          <div className='h-[18rem] overflow-y-scroll scroll-smooth flex flex-col gap-3 snap-y snap-mandatory'>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+            <div className='snap-start'><TaskItems /></div>
+          </div>
+
+        </form>
+
+        {/* Random quotes */}
+        <div className='mt-2'>
+          <p><em><strong>Your remaning todos : 3</strong></em></p>
+          <p><em>"Doing what you love is the comerstone of having abundance in your life" - Wayne Dyer</em></p>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Tasks
